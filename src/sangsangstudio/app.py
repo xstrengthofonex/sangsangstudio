f0e1187e763a0ea61b8859ad3dd04122050b44d0
Vincent White <xstrengthofonex@gmail.com>
2025-02-25 15:02:10 +0900

    Add templates and settings

--- src/sangsangstudio/app.py
+++ src/sangsangstudio/app.py
@@ -1,11 +1,45 @@
 import sys
+from abc import ABC, abstractmethod
 
-from falcon import App
+from falcon import App, Request, Response, HTTP_OK
+from jinja2 import Environment, FileSystemLoader
 from waitress import serve
+
+from src.sangsangstudio.settings import TEMPLATES_DIR
+
+
+class TemplateView(ABC):
+    @abstractmethod
+    def render(self, name: str, *args, **kwargs) -> str:
+        pass
+
+
+
+class Jinja2TemplateView(TemplateView):
+    def __init__(self, path: str):
+        self.env = Environment(loader=FileSystemLoader(path))
+
+    def render(self, name: str, *args, **kwargs) -> str:
+        t = self.env.get_template(name)
+        return t.render(*args, **kwargs)
+
+
+class HomeResource:
+    def __init__(self, view: TemplateView):
+        self.view = view
+
+    def on_get(self, req: Request, res: Response):
+        res.status = HTTP_OK
+        res.content_type = "text/html"
+        res.text = self.view.render("home.html")
 
 
 def create_app():
-    return App()
+    app = App()
+    view = Jinja2TemplateView(TEMPLATES_DIR)
+    home_resource = HomeResource(view)
+    app.add_route("/", home_resource)
+    return app
 
 
 def run_server(app, host: str = "localhost", port: int = 8080):
---
+++ src/sangsangstudio/settings.py
@@ -0,0 +1,5 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATES_DIR = os.path.join(ROOT_DIR, "templates")
+STATIC_DIR = os.path.join(ROOT_DIR, "static")
