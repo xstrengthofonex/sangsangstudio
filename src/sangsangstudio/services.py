6574ec8e024616cb60b938ad1f1f3918084c9041
Vincent White <xstrengthofonex@gmail.com>
2025-02-27 18:13:51 +0900

    Add PasswordHasher

--- src/sangsangstudio/services.py
+++ src/sangsangstudio/services.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 from sangsangstudio.entities import User
@@ -16,9 +17,38 @@ class UserDto:
     username: str
 
 
+class UnauthorizedLogin(RuntimeError):
+    pass
+
+
+@dataclass(frozen=True)
+class LoginRequest:
+    username: str
+    password: str
+
+
+class PasswordHasher(ABC):
+    @abstractmethod
+    def hash(self, password: str) -> bytes:
+        pass
+
+    @abstractmethod
+    def check(self, password: str, hashed: bytes) -> bool:
+        pass
+
+
+class BcryptPasswordHasher(PasswordHasher):
+    def hash(self, password: str) -> bytes:
+        pass
+
+    def check(self, password: str, hashed: bytes) -> bool:
+        pass
+
+
 class UserService:
     def __init__(self, repository: Repository):
         self.repository = repository
+        self.password_hasher = BcryptPasswordHasher()
 
     def create_user(self, request: CreateUserRequest) -> UserDto:
         user = User(username=request.username)
@@ -28,3 +58,8 @@ class UserService:
     def find_user(self, user_id: int) -> UserDto:
         user = self.repository.find_user(user_id)
         return UserDto(id=user.id, username=user.username)
+
+    def login(self, request):
+        raise UnauthorizedLogin
+
+
--- tests/test_user_service.py
+++ tests/test_user_service.py
@@ -1,22 +1,30 @@
 import pytest
 
 from sangsangstudio.services import (
-    CreateUserRequest, UserDto, UserService)
+    CreateUserRequest, UserDto, UserService, UnauthorizedLogin, LoginRequest)
 
 
 @pytest.fixture
 def user_service(repository):
     return UserService(repository)
 
+@pytest.fixture
+def user_password():
+    return "&tb&2l(@WqE&u"
 
 @pytest.fixture
-def a_user(user_service) -> UserDto:
+def a_user(user_service, user_password) -> UserDto:
     request = CreateUserRequest(
         username="a_user",
-        password="&tb&2l(@WqE&u")
+        password=user_password)
     return user_service.create_user(request)
 
 
-def test_user(user_service, a_user):
+def test_create_user(user_service, a_user):
     assert user_service.find_user(a_user.id) == a_user
 
+
+def test_login_fail(user_service, a_user, user_password):
+    with pytest.raises(UnauthorizedLogin):
+        request = LoginRequest(username=a_user.username, password=user_password)
+        user_service.login(request)
